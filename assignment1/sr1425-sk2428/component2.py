3edf01d29305506f83401ff79501067ebc06d522
Shivangi Rohilla <sr1425@ilabu.cs.rutgers.edu>
2024-09-25 21:23:36 -0400

    component 2 progress

--- assignment1/sr1425-sk2428/component2.py
+++ assignment1/sr1425-sk2428/component2.py
@@ -1,4 +1,5 @@
 import numpy as np # type: ignore
+import matplotlib.pyplot as plt # type: ignore
 
 def random_rotation_matrix(naive):
     if naive:
@@ -28,11 +29,11 @@ def random_rotation_matrix(naive):
     
     theta = 2 * np.pi * np.random.random()
     phi = 2 * np.pi * np.random.random()
-    x = 2 * np.pi * np.random.random()
+    x = np.random.random()
 
     V = [np.cos(phi) * np.sqrt(x), np.cos(theta) * np.sqrt(x), np.sqrt(1-x)]
 
-    temp = 2 * np.matmul(V, V.T)
+    temp = 2 * np.matmul(V, np.transpose(V))
     temp2 = np.subtract(temp, np.eye(3))
     result = np.matmul(temp2, [[np.cos(theta), np.sin(theta), 0],
                                [-np.sin(theta), np.cos(theta), 0],
@@ -41,14 +42,46 @@ def random_rotation_matrix(naive):
 
 def random_quaternion(naive):
     if naive:
-        return np.array()
+        v = np.random.random((4,0))
+        
+        norm_v = np.linalg.norm(v)
+
+        v = v / norm_v
+
+        return v
     
     vx = np.random.random()
     vy = np.random.random()
     theta = 2 * np.pi * np.random.random()
 
-    return np.array(np.cos(theta/2), vx*np.sin(theta/2), vy*np.sin(theta/2), np.sin(theta/2))
+    return [np.cos(theta/2), vx*np.sin(theta/2), vy*np.sin(theta/2), np.sin(theta/2)]
+
+def visualize_rotation(rotation_mat):
+    v0 = [0,0,1]
+
+    x_values = []
+    y_values = []
+    z_values = []
+
+    for i in range(300):
+        v1 = np.add([0,np.random.random(), 0], v0)
+        v0_prime = np.matmul(rotation_mat, v0)
+        v1_prime = np.matmul(rotation_mat, np.subtract(v1,v0))
+        x, y, z = np.subtract(v0_prime,v1_prime)
+        x_values.append(x)
+        y_values.append(y)
+        z_values.append(z)
+
+
+    fig = plt.figure(figsize=(12, 12))
+    ax = fig.add_subplot(projection='3d')
+    ax.scatter3D(x_values, y_values, z_values)
+
+    fig.savefig("visualize.png")
+
 
 #__name__
 if __name__=="__main__":
-    print("Hello World!")
+    R = random_rotation_matrix(True)
+
+    visualize_rotation(R)
