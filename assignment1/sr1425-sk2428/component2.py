83c67a88386e8ffd0d2996f302d1b60c254f1f50
Shivam Kajaria <shivam.kajaria@rutgers.edu>
2024-09-26 17:26:00 -0400

    fixed visualization

--- assignment1/sr1425-sk2428/component2.py
+++ assignment1/sr1425-sk2428/component2.py
@@ -27,18 +27,23 @@ def random_rotation_matrix(naive):
 
         return np.matmul(Rx, np.matmul(Ry, Rz))
     
-    theta = 2 * np.pi * np.random.random()
-    phi = 2 * np.pi * np.random.random()
-    x = np.random.random()
+    pi2 = 2 * np.pi
+    x_1 = np.random.random()
+    x_2 = np.random.random()
+    x_3 = np.random.random()
 
-    V = [np.cos(phi) * np.sqrt(x), np.cos(theta) * np.sqrt(x), np.sqrt(1-x)]
+    R = [[np.cos(pi2 * x_1), np.sin(pi2 * x_1), 0],
+         [-np.sin(pi2 * x_1), np.cos(pi2 * x_1), 0],
+         [0, 0, 1]]
 
-    temp = 2 * np.matmul(V, np.transpose(V))
-    temp2 = np.subtract(temp, np.eye(3))
-    result = np.matmul(temp2, [[np.cos(theta), np.sin(theta), 0],
-                               [-np.sin(theta), np.cos(theta), 0],
-                               [0,0,1]])
-    return result
+    v = np.array([np.cos(pi2 * x_2) * x_3 ** 0.5, np.sin(pi2 * x_2) * x_3 ** 0.5, (1-x_3)**0.5])
+    v = v[:, np.newaxis]
+
+    H = 2 * v @ np.transpose(v) - np.identity(3)
+
+    M = -H @ R
+    
+    return M 
 
 def random_quaternion(naive):
     if naive:
@@ -56,32 +61,46 @@ def random_quaternion(naive):
 
     return [np.cos(theta/2), vx*np.sin(theta/2), vy*np.sin(theta/2), np.sin(theta/2)]
 
-def visualize_rotation(rotation_mat):
-    v0 = [0,0,1]
+def plot_sphere(ax):
+    """Plot a sphere."""
+    phi = np.linspace(0, np.pi, 1000)
+    theta = np.linspace(0, 2 * np.pi, 1000)
+    x = np.outer(np.sin(phi), np.cos(theta))
+    y = np.outer(np.sin(phi), np.sin(theta))
+    z = np.outer(np.cos(phi), np.ones_like(theta))
 
-    x_values = []
-    y_values = []
-    z_values = []
-
-    for i in range(300):
-        v1 = np.add([0,np.random.random(), 0], v0)
-        v0_prime = np.matmul(rotation_mat, v0)
-        v1_prime = np.matmul(rotation_mat, np.subtract(v1,v0))
-        x, y, z = np.subtract(v0_prime,v1_prime)
-        x_values.append(x)
-        y_values.append(y)
-        z_values.append(z)
+    ax.plot_surface(x, y, z, color='b', alpha=0.3, rstride=5, cstride=5)
 
 
+def visualize_rotation(path="visualize.png", naive=False, n=100, show=True):
     fig = plt.figure(figsize=(12, 12))
-    ax = fig.add_subplot(projection='3d')
-    ax.scatter3D(x_values, y_values, z_values)
+    ax = fig.add_subplot(111, projection='3d') 
+    plot_sphere(ax)
+    ax.axes.set_xlim3d(left=-1, right=1) 
+    ax.axes.set_ylim3d(bottom=-1, top=1) 
+    ax.axes.set_zlim3d(bottom=-1, top=1) 
 
-    fig.savefig("visualize.png")
+    ax.axes.set_xlabel('x')
+    ax.axes.set_ylabel('y')
+    ax.axes.set_zlabel('z')
+
+    for i in range(n):
+        R = random_rotation_matrix(naive)
+        v_0 = np.array([[0], [0], [1]])
+        v_1 = np.array([[0], [0.01], [0]]) + v_0
+
+        v_0_p = R @ v_0
+        v_1_p = R @ (v_1 -  v_0)
+
+        ax.quiver3D(*v_0_p, *v_1_p, length=np.linalg.norm(v_1_p-v_0_p)*10, color='green')
+
+    if show:
+        plt.show()
+    fig.savefig(path)
 
 
-#__name__
-if __name__=="__main__":
-    R = random_rotation_matrix(True)
-
-    visualize_rotation(R)
+if __name__ == "__main__":
+    # visualize_rotation()
+    visualize_rotation("component_2_uniform.png", False, 1000, False)
+    visualize_rotation("component_2_naive.png", True, 1000, False)
+    
